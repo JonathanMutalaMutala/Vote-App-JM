8fd0744a9b4a45afe77bbd84db69460d408b7924
JonathanMutala <jonamut12.live@gmail.com>
2023-04-29 22:24:14 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vote_Application_JonathanMutala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
