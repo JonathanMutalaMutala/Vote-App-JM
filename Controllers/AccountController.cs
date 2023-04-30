229685bc7e5cd86a8a40e40ff5f7f3983f081853
JonathanMutala <jonamut12.live@gmail.com>
2023-04-29 23:46:24 -0300

    Add Login Controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vote_Application_JonathanMutala.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, ILogger<AccountController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
