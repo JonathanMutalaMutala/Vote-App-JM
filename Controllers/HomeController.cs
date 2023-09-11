1fa0567059a79f03705d1a4caf7c05c9a2ebb773
Mutala Mutala <jonathanmutala02@gmail.com>
2023-09-11 01:13:01 -0400

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,20 @@ namespace Vote_Application_JonathanMutala.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-		
+        #region Gestion de changement de langues
+        [AllowAnonymous]
+		[Route("/")]
+		[HttpPost]
+		public IActionResult ChangeLanguage(string culture, string returnUrl)
+		{
+			Response.Cookies.Append(
+				CookieRequestCultureProvider.DefaultCookieName,
+				CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+				new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+			);
+
+			return LocalRedirect(returnUrl);
+		}
+		#endregion
 	}
 }
